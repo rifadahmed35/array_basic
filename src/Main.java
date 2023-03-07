4d025a4a37a1e7b1b30070086d0be826123ca978
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-07 23:11:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a[] = {1, 20, 13, -10, 22};
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(a[i]);
+        }
+        {
+            System.out.println("The length of array:" + a.length);
+        }
+    }
+}
